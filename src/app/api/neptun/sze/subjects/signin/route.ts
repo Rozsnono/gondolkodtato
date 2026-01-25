64c43b1897cc217ebbc4836474e0dc06dcc83327
rozsnono <rozsnono@gmail.com>
2026-01-25 17:17:02 +0100

    fixes

--- src/app/api/neptun/sze/subjects/signin/route.ts
+++ src/app/api/neptun/sze/subjects/signin/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: Request) {
                 
                 const res = await response.json();
                 if (!response.ok) {
-                    return { error: response.status, message: res.notification[0].description };
+                    return { error: response.status, message: res.notification[0].description, item: item };
                 }
 
 
